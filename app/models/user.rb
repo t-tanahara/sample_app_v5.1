c9e3ca6b570952d8d1f33bfd1799b752cbd7a776
棚原拓也 <tanahara.2361@gmail.com>
2018-05-13 20:37:32 +0900

    emailの一意チェック

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
     validates :name,  presence: true, length: { maximum: 50 }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-    validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, length: { maximum: 255 }
+    validates :email,
+        presence: true,
+        format: { with: VALID_EMAIL_REGEX },
+        length: { maximum: 255 },
+        uniqueness: { case_sensitive: false }
 end
