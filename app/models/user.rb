abb7e1be049e0594c93a287a91212ca40dd4bde2
棚原拓也 <tanahara.2361@gmail.com>
2018-07-05 00:51:19 +0900

    docker/mysql構成（途中コミット）

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
     attr_accessor :remember_token
     before_save { self.email = email.downcase }
-    validates :name,  presence: true, length: { maximum: 50 }
+    validates :name,
+        presence: true,
+        length: { maximum: 50 }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email,
         presence: true,
@@ -9,7 +11,10 @@ class User < ApplicationRecord
         length: { maximum: 255 },
         uniqueness: { case_sensitive: false }
     has_secure_password
-    validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+    validates :password,
+        presence: true,
+        length: { minimum: 6 },
+        allow_nil: true
 
     # 渡された文字列のハッシュ値を返す
     def User.digest(string)
